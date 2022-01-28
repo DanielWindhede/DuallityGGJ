f73bbd3b4f5b2e263114ddb16e8fb4114f191ff3
Alexander <alexander.m.1999@gmail.com>
2022-01-29 00:05:51 +0100

    ceated PlayerController, bad PlatformSpawner, bad ScoreController

---
+++ Assets/Scripts/ScoreControllerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreControllerScript : MonoBehaviour {
+
+    static Text scoreText;
+
+    static int score;
+
+    static float mStartPos;
+    public static float startPos { get { return mStartPos; } set { mStartPos = value; } }
+
+    private void Awake()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    void Start ()
+    {
+        score = 0;
+        UpdateScore();
+	}
+
+    public static void addScore(float addedScore)
+    {
+        score = (int)((addedScore - startPos) * 10);
+        UpdateScore();
+    }
+
+    static void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
