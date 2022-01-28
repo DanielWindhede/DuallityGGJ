7aed781057669a3ade802d9c535bb5053a120b62
Alexander <alexander.m.1999@gmail.com>
2022-01-29 00:02:23 +0100

    State Machine

---
+++ Assets/Scripts/State Machine/State.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State<T> {
+
+    protected T owner;
+
+    public State() {
+    }
+
+    public State(T owner) {
+        this.owner = owner;
+    }
+
+    virtual public void Enter() {
+
+        //
+    }
+
+    virtual public void Execute() {
+    }
+
+    virtual public void FixedExecute() {
+    }
+
+    virtual public void Exit() {
+    }
+
+}
---
+++ Assets/Scripts/State Machine/StateMachine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine<T> {
+
+    private State<T> currentlyRunningState;
+    private State<T> previousState;
+
+
+    public State<T> CurrentlyRunningState {
+        get {
+            return currentlyRunningState;
+        }
+    }
+
+    public void ChangeState(State<T> newState) {
+
+        if (this.currentlyRunningState != null)
+            this.currentlyRunningState.Exit();
+
+        this.previousState = this.currentlyRunningState;
+
+        this.currentlyRunningState = newState;
+
+        this.currentlyRunningState.Enter();
+    }
+
+    public void Update() {
+        if (currentlyRunningState != null)
+            currentlyRunningState.Execute();
+    }
+
+    public void FixedUpdate() {
+        if (currentlyRunningState != null)
+            currentlyRunningState.FixedExecute();
+    }
+
+    public void SwitchToPreviousState() {
+        if (this.currentlyRunningState != null)
+            this.currentlyRunningState.Exit();
+
+        this.currentlyRunningState = previousState;
+        this.currentlyRunningState.Enter();
+    }
+}
