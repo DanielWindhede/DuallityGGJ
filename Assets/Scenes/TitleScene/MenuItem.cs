01f70c0d03c7574b3088de1c00b8e20ae63edae4
nathalie <nathaliet1099@gmail.com>
2022-01-30 00:24:36 +0100

    Added Sounds

--- Assets/Scenes/TitleScene/MenuItem.cs
+++ Assets/Scenes/TitleScene/MenuItem.cs
@@ -2,16 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
 
 public class MenuItem : MonoBehaviour
 {
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] GameObject sprite;
+    [SerializeField] GameObject sprite1;
     public enum MenuActionType {
         Play,
         Quit,
     }
 
     [SerializeField] private MenuActionType type;
-     public void PerformAction() {
+
+    public void PlaySound()
+    {
+        audioSource.Play();
+    }
+
+    public void ChangeSprite()
+    {
+
+    }
+
+    public void ChangeSpriteBack()
+    {
+
+    }
+
+    public void PerformAction() {
         switch (type) {
             case MenuActionType.Play:
                 SceneManager.LoadScene(1);
